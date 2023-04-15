0ef4808e5d3d68a0b2a0d1b77836e19b53a634ec
loi02112001 <loi65732102@gmail.com>
2023-04-15 20:19:49 +0700

    create-authen-author

--- src/routes/v1/adminRoute.js
+++ src/routes/v1/adminRoute.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
+const {verifyTokenAdmin} = require('../../middlewares/verify')
 const adminController = require("../../controllers/adminController");
 const uploadFile = require("../../middlewares/upload");
 
 router.post("/", adminController.createAdminAcc);
 
-router.put("/:id", uploadFile, adminController.updateAdmin);
+router.put("/:id",verifyTokenAdmin, uploadFile, adminController.updateAdmin);
+
+router.post("/login",adminController.login)
+
+router.post("/refresToken",adminController.refreshToken)
+
+router.post("/logout",adminController.logOut)
 
 module.exports = router;
--- src/routes/v1/carRoute.js
+++ src/routes/v1/carRoute.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const carController = require("../../controllers/carController");
 const uploadFile = require("../../middlewares/upload");
+const { verifyTokenAdmin, verifyTokenAllRole } = require("../../middlewares/verify");
 
-router.get("/", carController.getAllCar);
-router.get("/detail", carController.getCarDetail);
+router.get("/",verifyTokenAllRole, carController.getAllCar);
+router.get("/detail",verifyTokenAllRole, carController.getCarDetail);
 
-router.post("/", carController.createCar);
+router.post("/",verifyTokenAdmin, carController.createCar);
 
-router.put("/", uploadFile('images'), carController.updateCar);
+router.put("/",verifyTokenAdmin, uploadFile('images'), carController.updateCar);
 
 module.exports = router;
