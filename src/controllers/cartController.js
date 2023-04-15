c5c98c50de7ced71438d7bfb0fa46d4391b2d2b2
DvMinhGithub <dvminh.it@gmail.com>
2023-04-15 15:46:49 +0700

    Update cart controller

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -5,20 +5,20 @@ const cartController = {
   //Thêm hoặc xoá sản phẩm trong giỏ hàng
   updateCart: async (req, res) => {
     try {
-      const { idCustomer } = req.params; //Id khách hàng
+      const { idCustomer } = req.params;
       const product = req.body;
 
-      for (i = 0; i < product.length; i++) {
-        const getCar = await carModel.findById(product[i].idProduct);
-        const getPrice = getCar.amountPrice;
-        product[i].price = getPrice;
-      }
-      
-      const totalPrice = product.reduce(
-        (totalValue, currentValue) =>
-          totalValue + currentValue.price * currentValue.amountProduct,
-        0
-      );
+      const carIds = product.map((p) => p.idProduct);
+      // lấy ra các document có _id thuộc carIds
+      const cars = await carModel.find({ _id: { $in: carIds } });
+
+      const totalPrice = cars.reduce((total, car) => {
+        // lấy ra amount product tương ứng với car hiện tại
+        const { amountProduct } = product.find(
+          (p) => p.idProduct === car._id.toString()
+        );
+        return total + car.amountPrice * amountProduct;
+      }, 0);
 
       const cartUpdate = await cartModel.findOneAndUpdate(
         { idCustomer },
@@ -28,6 +28,7 @@ const cartController = {
         },
         { new: true }
       );
+
       res.status(200).json({ success: true, data: cartUpdate });
     } catch (error) {
       res.status(500).json({ success: false, message: error.message });
