21f30ed7b4dbc03200b52a66d6a97535725428e8
Minh Dinh <minh.dinh@solislab.com>
2023-08-03 10:35:38 +0700

    Fix security holes

--- server.js
+++ server.js
@@ -1,35 +1,21 @@
 require("dotenv").config();
-
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const express = require("express");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
-const morgan = require("morgan");
 const routes = require("./src/routes");
 const connectDb = require("./src/configs/db");
 const app = express();
-
 connectDb();
-
-// Mdiddleware configuration
 app.use(helmet());
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  morgan("dev", {
-    skip: function (req, res) { return res.statusCode < 400 }
-  })
-);
-
 app.use("/", express.static("public"));
-
 app.use(routes);
-
 const { PORT } = process.env || 8080;
-
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
